49294086497a9ce25aeb4b2b3629e51ab9e4debb
kluzko <kluzniakkuba@gmail.com>
2023-06-03 15:10:05 +0200

    Add project files.

---
+++ Zaj2/Zad_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj trzy liczby:");
+
+        Console.Write("Liczba 1: ");
+        double liczba1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Liczba 2: ");
+        double liczba2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Liczba 3: ");
+        double liczba3 = Convert.ToDouble(Console.ReadLine());
+
+        double srednia = (liczba1 + liczba2 + liczba3) / 3;
+
+        Console.WriteLine("Średnia arytmetyczna wynosi: " + srednia);
+
+        Console.ReadLine();
+    }
+}
